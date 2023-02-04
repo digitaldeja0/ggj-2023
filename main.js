7059ac62fa06a5ed61710145b34e844e342ef00c
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2023-02-03 23:17:05 -0500

    File setup.

---
+++ main.js
@@ -0,0 +1,60 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// Canvas
+const canvas = document.querySelector("#canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Scene, Camere, Renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  59,
+  canvas.width / canvas.height,
+  0.1,
+  100
+);
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
+renderer.setSize(canvas.width, canvas.height);
+document.body.appendChild(renderer.domElement);
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+// Light
+const light = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add(light);
+// Update Camera
+camera.position.z = 3;
+// Add Cube
+const box = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+const cube = new THREE.Mesh(box, material);
+scene.add(cube);
+
+
+// Animation Loop
+let startTimeStamp = Date.now();
+
+function animate() {
+  const endTimeStamp = Date.now();
+  const elapsedTime = endTimeStamp - startTimeStamp;
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  camera.updateProjectionMatrix();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
+
+// Page Resizer
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  })
