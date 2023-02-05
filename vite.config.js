cc9e124d3ec2946765aa74c7ba707f5d5ca3ce4d
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2023-02-05 17:27:03 -0500

    Fixed file roots.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         help: resolve(__dirname, 'help.html'),
+        end: resolve(__dirname, 'end.html'),
+        paly: resolve(__dirname, 'play.html'),
       },
     },
   },
